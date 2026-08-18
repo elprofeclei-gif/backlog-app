1cf7655a5e4ed2aa05ac8692d263ef38b4575b07
elprofeclei-gif <elprofeclei@gmail.com>
2026-08-18 14:42:34 -0500

    Ci: Add GitHub Actions for frontend testing

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,5 +1,5 @@
 /// <reference types="vitest" />
-import { defineConfig } from 'vite';
+import { defineConfig } from 'vitest/config'; // <-- Cambiado de 'vite' a 'vitest/config'
 import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
